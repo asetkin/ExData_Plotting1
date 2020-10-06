9e380757e4de57055c790bb857342be6be792198
asetkin <aset@kineyev.com>
2020-10-06 11:13:49 +0600

    second try with one screen device

---
+++ plot3.R
@@ -0,0 +1,33 @@
+plot3<-function(){
+#read data from downloaded file
+alldata<-read.table("household_power_consumption.txt",skip=1,sep=";")
+#rename 9 variables with names from the description 
+names(alldata)<-c("Date","Time","Global_active_power","Global_reactive_power","Voltage",
+                    "Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+#subset data from the dates 2007-02-01 and 2007-02-02
+data<-subset(alldata,alldata$Date=="1/2/2007" | alldata$Date =="2/2/2007")
+#delete unnecessary data, for cleaning memory
+rm(alldata)
+
+#changing date format to Date format (dd.mm.yy)
+data$Date<-as.Date(data$Date, format="%d/%m/%Y")
+#changing time format to POSIXlt
+data$Time<-strptime(data$Time, format="%H:%M:%S")
+data[1:1440,"Time"] <- format(data[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+data[1441:2880,"Time"] <- format(data[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+#write plot3 to plot3.png
+png("plot3.png", width=480, height=480)
+#creating empty plot
+plot(data$Time,data$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+#adding variable 1 with black colored line
+with(data,lines(Time,as.numeric(as.character(Sub_metering_1)), col="black"))
+#adding variable 2 with red colored line
+with(data,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+#adding variable 3 with blue colored line
+with(data,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+#adding legend
+legend("topright", cex = 1.2, lty=1, col=c("black","red","blue"),
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
+}
